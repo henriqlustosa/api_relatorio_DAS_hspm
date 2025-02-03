2adef159029c7b638d4c1a8c47e3e1fe32b7124a
henriqlustosa <henriqlustosa@gmail.com>
2025-02-03 15:59:16 -0300

    Inicio do projeto de API para apresentar o relatorio do DAS

---
+++ src/main/java/HSPM/api_relatorio_das/ApiRelatorioDasApplication.java
@@ -0,0 +1,13 @@
+package HSPM.api_relatorio_das;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRelatorioDasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRelatorioDasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HSPM/api_relatorio_das/ApiRelatorioDasApplicationTests.java
@@ -0,0 +1,13 @@
+package HSPM.api_relatorio_das;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRelatorioDasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
